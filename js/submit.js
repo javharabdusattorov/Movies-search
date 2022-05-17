7089fc43dad7a3a3ca8b884f121e70379072e9e0
javharabdusattorov <javharabdusattorov2008@gmail.com>
2022-05-18 00:28:52 +0500

    auto type

--- js/submit.js
+++ js/submit.js
@@ -1,9 +1,17 @@
 
 $('form.search-form').submit(function(evt) {
     evt.preventDefault();
+    $('.header-things h2').text('Movies App');
     let inputValue = $('.form__search').val();
     search = inputValue;
     searchFetch();
     $('.form__search').val("");
     $('.form__search').attr('placeholder', search);
 });
+
+let typed = new Typed(".auto-type", {
+    strings: ["Search your movie,", " Then click like", "and Watch it in LIKE movies", "THEN enjoy!!!"],
+    typeSpeed: 130,
+    backSpeed: 70,
+    loop: true
+})
